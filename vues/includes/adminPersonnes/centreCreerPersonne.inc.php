0fd6186f340566338ceda067fb5e873f7372e427
btssio <btssio@btssio-VirtualBox>
2014-10-16 16:31:06 +0200

    modifiacation du css intégrée

--- vues/includes/adminPersonnes/centreCreerPersonne.inc.php
+++ vues/includes/adminPersonnes/centreCreerPersonne.inc.php
@@ -53,7 +53,7 @@
 
     <!-- Information nécessaire uniquement aux étudiants -->
 
-    <div id="Formulaire_Etudiant" style="visibility:hidden" height="0">
+    <div id="Formulaire_Etudiant" style="display:none" height="0">
         <fieldset>
             <legend>Informations specifiques aux étudiant</legend>
 
@@ -83,7 +83,7 @@
 
     <!-- Information nécessaire uniquement aux maitre de stage -->
 
-    <div id="Formulaire_MaitreStage" style="visibility:hidden" height="0">
+    <div id="Formulaire_MaitreStage" style="display:none" height="0">
         <fieldset>
             <legend>Choisir l'entreprise :</legend>
 
